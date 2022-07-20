5da9e1c15bc58b659f80e82e9a90635b66572d1d
SunGod7 <sun7ruler@gmail.com>
2022-07-20 11:17:45 -0400

    added heroku

--- models/connection.js
+++ models/connection.js
@@ -1,23 +1,20 @@
 require('dotenv').config()
 // getting mongoose to use
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
+// Fire off the connection to Mongo DB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-const DATABASE_URI = process.env.DATABASE_URI
-const config = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connected to ${mongoose.connection.host}:${mongoose.connection.port}`);
+});
 
-// connecting our mongoDB to mongoose
-mongoose.connect(DATABASE_URI, config)
+mongoose.connection.on("error", (err) => {
+  console.log("Could not connect to MongoDB!", err);
+});
 
-mongoose.connection
-
-    .on('open', () => console.log('Connected to Mongoose'))
-    
-    .on('close', () => console.log('Disconnected from Mongoose'))
-    
-    .on('error', err => console.error(err))
 
 module.exports = mongoose
