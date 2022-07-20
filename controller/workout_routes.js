374f2432fe94354e2a91c61c1f0e748c38dce6f2
SunGod7 <sun7ruler@gmail.com>
2022-07-20 14:55:02 -0400

    route changes

--- controller/workout_routes.js
+++ controller/workout_routes.js
@@ -114,14 +114,14 @@ router.get('/', (req, res) => {
 router.get('/workseed', (req, res) => {
 
     const pickWorkouts = [
-        {trainning: 'Beginner Lower Body', exercise: 'Air squats and lunges', reps: '20, 10', sets: '3', rest: '60', warmup: '50 jumping jack and stretching', frequency: '3 times per week'},
-        {trainning: 'Beginner Upper Body', exercise: 'Push-ups and Dips ', reps: '15-25, 8-12', sets: '5', rest: '90', warmup: '50 jumping jack and stretching', frequency: '3 times per week'},
-        {trainning: 'Beginner Cardio', exercise: '0ne hr on treadmill. walk or run ', reps: '', sets: '', rest: 'rest when your finished', warmup: '50 jumping jack and stretching', frequency: '4 times per week'},
-        {trainning: 'Beginner stomache', exercise: 'reverse crunch, oblique crunch', reps: '20-25, 15-20', sets: '5', rest: '90', warmup: '50 jumping jack and stretching', frequency: '3 times per week'},
-        {trainning: 'Advanced Lower Body', exercise: 'weighted squats, deadlift', reps: '8-10, 10-12', sets: '5', rest: '90', warmup: '100 Air squats ', frequency: '4 times per week'},
-        {trainning: 'Advanced Upper Body', exercise: 'bench press, dumdbell curls', reps: '8-12, 10-12', sets: '5', rest: '90', warmup: '100 push-ups', frequency: '4 times per week'},
-        {trainning: 'Advanced Cardio', exercise: 'Burpees', reps: '100', sets: '3', rest: '2 min', warmup: '50 push-ups, 50 jumping jack', frequency: '4 times per week'},
-        {trainning: 'Advanced stomache ', exercise: 'sit-ups, pull-ups', reps: '30-40, 10-15', sets: '12', rest: '60', warmup: '50 jumping jack', frequency: '4 times per week'},
+        {trainnig: 'Beginner Lower Body', exercise: 'Air squats and lunges', reps: '20, 10', sets: '3', rest: '60', warmup: '50 jumping jack and stretching', frequency: '3 times per week'},
+        {trainnig: 'Beginner Upper Body', exercise: 'Push-ups and Dips ', reps: '15-25, 8-12', sets: '5', rest: '90', warmup: '50 jumping jack and stretching', frequency: '3 times per week'},
+        {trainnig: 'Beginner Cardio', exercise: '0ne hr on treadmill. walk or run ', reps: '', sets: '', rest: 'rest when your finished', warmup: '50 jumping jack and stretching', frequency: '4 times per week'},
+        {trainnig: 'Beginner stomache', exercise: 'reverse crunch, oblique crunch', reps: '20-25, 15-20', sets: '5', rest: '90', warmup: '50 jumping jack and stretching', frequency: '3 times per week'},
+        {trainnig: 'Advanced Lower Body', exercise: 'weighted squats, deadlift', reps: '8-10, 10-12', sets: '5', rest: '90', warmup: '100 Air squats ', frequency: '4 times per week'},
+        {trainnig: 'Advanced Upper Body', exercise: 'bench press, dumdbell curls', reps: '8-12, 10-12', sets: '5', rest: '90', warmup: '100 push-ups', frequency: '4 times per week'},
+        {trainnig: 'Advanced Cardio', exercise: 'Burpees', reps: '100', sets: '3', rest: '2 min', warmup: '50 push-ups, 50 jumping jack', frequency: '4 times per week'},
+        {trainnig: 'Advanced stomache ', exercise: 'sit-ups, pull-ups', reps: '30-40, 10-15', sets: '12', rest: '60', warmup: '50 jumping jack', frequency: '4 times per week'},
 
 
 
